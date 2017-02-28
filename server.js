52c687b3415d7751ce462a456a64fcd3b1640316
Viktor Lesic <weekaah@gmail.com>
2017-02-28 17:28:56 +0100

    add remote mongodb connection

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-// grab dependecies
+// load environment variables =========
+require('dotenv').config();
+
+// grab dependecies ===================
 const express = require('express'),
       app = express(),
       port = process.env.PORT || 8080,
-      expressLayouts = require('express-ejs-layouts');
+      expressLayouts = require('express-ejs-layouts'),
+      mongoose = require('mongoose');
       
-// configure application
+// configure application ==============
 // tell express where to look for static assets
 app.use(express.static(__dirname + 'public'));
+
 // set ejs as templating engine
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
-// set the routes
+// connect to our database
+mongoose.connect(process.env.DB_URI);
+
+// set the routes =====================
 app.use(require('./app/routes'));
 
-// start our server
+// start our server ===================
 app.listen(port, () => {
   console.log(`App listening on http://localhost:${port}`);
 });
