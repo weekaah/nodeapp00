b63cfcbd58635d9d539a864e397d3b3167160e2c
Viktor Lesic <weekaah@gmail.com>
2017-02-28 15:05:40 +0100

    configure server.js to start server

---
+++ server.js
@@ -0,0 +1,16 @@
+// grab dependecies
+const express = require('express'),
+      app = express(),
+      port = process.env.PORT || 8080;
+      
+// configure application
+
+// set the routes
+app.get('/', (req, res) => {
+  res.send('Hello, I am the app');
+});
+
+// start our server
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
